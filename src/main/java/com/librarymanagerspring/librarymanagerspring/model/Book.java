9cd320d5cffe80c87d8d22ce3788307800727db9
Ronalyn <ronalynnanong@live.com>
2021-05-10 17:08:34 +0100

    unit testing for get, add, update, delete done

--- src/main/java/com/librarymanagerspring/librarymanagerspring/model/Book.java
+++ src/main/java/com/librarymanagerspring/librarymanagerspring/model/Book.java
@@ -1,12 +1,16 @@
 package com.librarymanagerspring.librarymanagerspring.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
-
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,7 +19,7 @@ import java.io.Serializable;
 public class Book implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
--- src/test/java/com/librarymanagerspring/librarymanagerspring/LibrarymanagerspringApplicationTests.java
+++ src/test/java/com/librarymanagerspring/librarymanagerspring/LibrarymanagerspringApplicationTests.java
@@ -1,13 +1,77 @@
 package com.librarymanagerspring.librarymanagerspring;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import com.librarymanagerspring.librarymanagerspring.model.Book;
+import com.librarymanagerspring.librarymanagerspring.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class LibrarymanagerspringApplicationTests {
 
+	@Autowired
+	BookRepository bookRepository;
+
 	@Test
-	void contextLoads() {
+	public void testAddBook(){
+		Book bookTestOne = new Book();
+		bookTestOne.setId(100L);
+		bookTestOne.setTitle("A Book About Unit Testing");
+		bookTestOne.setAuthor("Ronalyn Nanong");
+		bookTestOne.setYear(2021L);
+		bookTestOne.setPages(100L);
+		bookRepository.save(bookTestOne);
+		assertNotNull(bookRepository.findById(100L).get());
+
+		Book bookTestTwo = new Book();
+		bookTestTwo.setId(101L);
+		bookTestTwo.setTitle("Unit Testing Update Book");
+		bookTestTwo.setAuthor("Ronalyn Nanong");
+		bookTestTwo.setYear(2021L);
+		bookTestTwo.setPages(100L);
+		bookRepository.save(bookTestTwo);
+		assertNotNull(bookRepository.findById(100L).get());
+
+		Book bookTestThree = new Book();
+		bookTestThree.setId(102L);
+		bookTestThree.setTitle("Unit Testing Delete Book");
+		bookTestThree.setAuthor("Ronalyn Nanong");
+		bookTestThree.setYear(2021L);
+		bookTestThree.setPages(101L);
+		bookRepository.save(bookTestThree);
+		assertNotNull(bookRepository.findById(101L).get());
+	}
+
+	@Test
+	public void getAllBooks(){
+		List<Book> bookList = bookRepository.findAll();
+		assertTrue(bookList.size() > (0));
+	}
+
+	@Test
+	public void testGetBookById () {
+		Book book = bookRepository.findById(100L).get();
+		assertEquals("A Book About Unit Testing", bookRepository.findById(100L).get().getTitle());
+		assertEquals("Ronalyn Nanong", bookRepository.findById(100L).get().getAuthor());
+	}
+
+	@Test
+	public void testUpdateBook () {
+		Book book = bookRepository.findById(101L).get();
+		book.setTitle("Updated Unit Testing Book!!");
+		bookRepository.save(book);
+		assertNotEquals("Unit Testing Update Book", bookRepository.findById(100L).get().getTitle());
+	}
+
+	@Test
+	public void testDelete(){
+		bookRepository.deleteById(102L);
+		assertFalse(bookRepository.existsById(102L));
 	}
 
 }
